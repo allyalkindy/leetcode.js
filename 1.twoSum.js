3c94e91763e860406211f6d60e7baaf91800f7c7
ally mohammed said <allymohammedsaid126@gmail.com>
2026-09-17 17:15:32 +0300

    two sum solved

--- 1.twoSum.js
+++ 1.twoSum.js
@@ -1,1 +1,22 @@
-console.logs("codes here !!")
+ function twoSum(arr, target) {
+ let seen = {
+
+ }
+
+
+ for(let i=0 ; i<arr.length ; i++){
+     const complement = target - arr[i] ;
+
+      if(seen.hasOwnProperty(complement)){
+        return [seen[complement],i]
+      }
+
+      seen[arr[i]] = i ;
+ }
+
+ return undefined ;
+}
+
+
+
+console.log(twoSum([2,9,3,6,7],25))
