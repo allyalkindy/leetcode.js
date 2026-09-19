6f74b6323f4b9198f02da4af616c2e9b04bc1758
ally mohammed said <allymohammedsaid126@gmail.com>
2026-09-19 20:00:38 +0300

    top k freq elements first solution

--- 4.topKFrequentElement.js
+++ 4.topKFrequentElement.js
@@ -1,19 +1,38 @@
 const topKFrequent = (nums,k) =>{
 
-let mostFrequent = {}
+let elementsCounts = {}
 
 for(let i = 0 ; i<nums.length ; i++){
-    mostFrequent[nums[i]] = (mostFrequent[nums[i]] || 0) + 1 ;
+    elementsCounts[nums[i]] = (elementsCounts[nums[i]] || 0) + 1 ;
+}
+
+const keys = Object.keys(elementsCounts).map(Number) ;
+const values = Object.values(elementsCounts) ;
+
+let frequents = [] ; 
+
+
+for(let i = 0 ; i<k ; i++ ){
+    const freqValue = Math.max(...values) ;
+    const valueIndex = values.indexOf(freqValue)
+    values.splice(valueIndex,1) ;
+
+    const freqKey = keys.find(k => elementsCounts[k] === freqValue)
+    const keyIndex = keys.indexOf(freqKey) ;
+
+    frequents.push(freqKey) ;
+    keys.splice(keyIndex,1) ; 
+
 }
 
 
-const values = Object.values(mostFrequent) ;
 
 
-return  values ;
+return frequents;
 
 
 
 }
 
 console.log(topKFrequent([1,1,1,2,2,3],2))
+console.log(topKFrequent([1,2,1,2,1,2,3,1,3,2],2))
