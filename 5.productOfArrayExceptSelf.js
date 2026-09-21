415eb8e30e7227cefd4da54a8e70cb0f31c2493a
ally mohammed said <allymohammedsaid126@gmail.com>
2026-09-21 18:09:49 +0300

    question 5 optimized

--- 5.productOfArrayExceptSelf.js
+++ 5.productOfArrayExceptSelf.js
@@ -2,25 +2,30 @@ const productExceptSelf = (nums) => {
 
 let results  = [] ;
 
+let left = 1
 for(let i = 0 ; i<nums.length; i++){
-    let curr = 1 ;
+   
+    results.push(left) ;
 
-    for(let k=0 ; k<nums.length ; k++){
-
-    if(!(k==i)){
-    curr = curr*nums[k] ;
-    }
+     left = left * nums[i] ;
+   
 
 }
-results.push(curr) ;
+
+
+let right = 1 ;
+
+for(let i = nums.length-1 ; i>=0 ; i--){
+    results[i] = results[i]*right ; 
+
+    right = right*nums[i] ;
 }
 
 
 
 return results ;
-
-
-
 }
 
-console.log(productExceptSelf([1,2,3,4])) ;
+
+
+console.log(productExceptSelf([-1,1,0,-3,3])) ;
